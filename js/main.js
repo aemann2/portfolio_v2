333d385672b3bcbbfa1f5c2b0f7eae6f238c43fa
Adam Mann <aemann2@gmail.com>
2021-05-25 15:39:54 -0700

    refactored hamburger, implemented mobile-first

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// hamburger menu
+
+const navLinks = document.querySelector('.navbar__links');
+
+function hamburgerMenu() {
+  if (navLinks.style.display === 'block') {
+    navLinks.style.display = 'none';
+  } else {
+    navLinks.style.display = 'block';
+  }
+}
+
+// function restoreNav() {
+//   if (window.innerWidth > 550) {
+//     navLinks.style.display = 'flex';
+//   } else navLinks.style.display = 'none';
+// }
+
+window.addEventListener('resize', restoreNav);
+
+// navbar color transition
+
+const navbar = document.querySelector('.navbar');
+
+window.onscroll = () => {
+  if (window.scrollY > window.innerHeight - 50) {
+    navbar.style.backgroundColor = 'rgb(22, 39, 89)';
+    navbar.style.mixBlendMode = 'normal';
+  } else {
+    navbar.style.backgroundColor = '';
+    navbar.style.mixBlendMode = 'difference';
+  }
+};
